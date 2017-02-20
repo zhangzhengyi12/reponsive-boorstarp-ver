ecfe7d2b10f2ee87bbc10874e83b0915ca861996
zhangzhengyi12 <zhangzhengyi12@live.com>
2017-02-20 18:36:53 +0800

    diff nav-height add nav-lcwz.less download less.main.less complate caroules add main.js

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Administrator on 2017/2/20.
+ */
+
+$(function () {
+    var $carousels = $('.carousel');
+    var startx ,endx;
+    var dis = 80;
+    //获取触摸参数
+    $carousels.on('touchstart',function (e) {
+        startx = e.originalEvent.touches[0].clientX
+    })
+    //获取开始的值
+
+    $carousels.on('touchmove',function (e) {
+        endx = e.originalEvent.touches[0].clientX
+    })
+    //通过变量不断的覆盖value 获取到最后一刻的x值
+    $carousels.on('touchend',function (e) {
+        if(Math.abs(startx - endx) > dis){
+            // startx > endx ? $carousels.carousel('next') : $carousels.carousel('prev');
+
+            // if(startx > endx ){
+            //     $carousels.carousel('next');
+            // }else if(startx < endx ){
+            //     $carousels.carousel('prev');
+            // }
+            //
+
+            startx > endx ? $carousels.carousel('next') : startx < endx ? $carousels.carousel('prev') : 1 ;
+        }
+
+    }) //在触摸完成后 触发对比事件 和bootstarp自带的翻页方法
+
+});
